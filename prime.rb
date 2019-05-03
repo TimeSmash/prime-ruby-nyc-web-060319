bc4d9445414783caa068f9cafa6f956b99666181
mcumm64 <mcumm64@gmail.com>
2019-05-03 21:24:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  if num < 0
+    false
+  end
+  #prime num = only factors are itself and 1
+  for x in (2..num-1)
+    if num % x == 0
+      return false
+    end
+end
